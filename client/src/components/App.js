a6ebe1a959c7dcac466ad3b4e379fff1780e346e
Burhan Mullamitha <burhanmm1998@gmail.com>
2020-06-18 03:11:44 +0100

    Created stream components and routing

---
+++ client/src/components/App.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import { BrowserRouter, Route, Link } from 'react-router-dom';
+import StreamCreate from './streams/StreamCreate';
+import StreamList from './streams/StreamList';
+import StreamEdit from './streams/StreamEdit';
+import StreamShow from './streams/StreamShow';
+import StreamDelete from './streams/StreamDelete';
+
+const App = () => {
+    return (
+        <BrowserRouter>
+            <div>
+                <Route path="/" exact component={StreamList} />
+                <Route path="/streams/new" component={StreamCreate} />
+                <Route path="/streams/edit" component={StreamEdit} />
+                <Route path="/streams/delete" component={StreamDelete} />
+                <Route path="/streams/show" component={StreamShow} />
+            </div>
+        </BrowserRouter>
+    );
+};
+
+export default App;
