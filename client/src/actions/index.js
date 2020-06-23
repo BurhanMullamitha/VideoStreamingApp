cb5ff3ce8e76634b8c89830b45934eb044eba3e0
Burhan Mullamitha <burhanmm1998@gmail.com>
2020-06-24 00:00:12 +0100

    Added actions and reducers

--- client/src/actions/index.js
+++ client/src/actions/index.js
@@ -1,4 +1,15 @@
-import { SIGN_IN, SIGN_OUT } from './types';
+import streams from '../components/apis/streams';
+import 
+    { 
+        SIGN_IN, 
+        SIGN_OUT, 
+        CREATE_STREAM,
+        FETCH_STREAMS,
+        FETCH_STREAM,
+        EDIT_STREAM,
+        DELETE_STREAM
+    } 
+from './types';
 
 export const signIn = (userId) => {
     return {
@@ -12,3 +23,28 @@ export const signOut = () => {
         type: SIGN_OUT
     };
 };
+
+export const createStream = formValues => async dispatch => {
+    const response = streams.post('/streams', formValues);
+    dispatch({ type: CREATE_STREAM, payload: response.data })
+};
+
+export const fetchStreams = () => async dispatch => {
+    const response = streams.get('/streams');
+    dispatch({ type: FETCH_STREAMS, payload: response.data })
+}
+
+export const fetchStream = (id) => async dispatch => {
+    const response = streams.get(`streams/${id}`);
+    dispatch({ type: FETCH_STREAM, payload: response.data })
+}
+
+export const editStream = (id, formValues) => async dispatch => {
+    const response = streams.put(`streams/${id}`, formValues);
+    dispatch({ type: EDIT_STREAM, payload: response.data })
+}
+
+export const deleteStream = (id) => async dispatch => {
+    await streams.delete(`steams/${id}`);
+    dispatch({ type: DELETE_STREAM, payload: id })
+}
