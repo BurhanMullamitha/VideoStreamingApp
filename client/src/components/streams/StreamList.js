2bc38356aa603b442c4af1964fe7b16e03e9ce3b
Burhan Mullamitha <burhanmm1998@gmail.com>
2020-06-24 17:43:59 +0100

    Added stream edit/delete/create buttons, implemented plain router for navigation

--- client/src/components/streams/StreamList.js
+++ client/src/components/streams/StreamList.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { connect } from 'react-redux';
+import { Link } from 'react-router-dom';
 import { fetchStreams } from '../../actions';
 
 class StreamList extends React.Component {
@@ -7,11 +8,26 @@ class StreamList extends React.Component {
         this.props.fetchStreams();
     }
 
+    renderAdmin(stream) {
+        if(stream.userId === this.props.currentUserId) {
+            return (
+                <div className="right floated content">
+                    <button className="ui button primary">
+                        Edit
+                    </button>
+                    <button className="ui button negative">
+                        Delete
+                    </button>
+                </div>
+            )
+        }
+    }
+
     renderList = () => {
-        console.log(this.props.streams);
         return this.props.streams.map(stream => {
             return (
                 <div className="item" key={stream.id}>
+                    {this.renderAdmin(stream)}
                     <i className="large middle aligned icon camera" />
                     <div className="content">
                         {stream.title}
@@ -23,6 +39,16 @@ class StreamList extends React.Component {
             );
         });
     }
+
+    renderCreate() {
+        if(this.props.isSignedIn) {
+            return (
+                <div style={{textAlign: "right"}}>
+                    <Link to="/streams/new" className="ui button primary">Create Stream</Link>
+                </div>
+            )
+        }
+    }
     
     render() {
         return (
@@ -31,13 +57,18 @@ class StreamList extends React.Component {
                 <div className="ui celled list">
                     {this.renderList()}
                 </div>
+                {this.renderCreate()}
             </div>
         );
     }
 };
 
 const mapStateToProps = (state) => {
-    return { streams: Object.values(state.streams) };
+    return { 
+        streams: Object.values(state.streams),
+        currentUserId: state.auth.userId,
+        isSignedIn: state.auth.isSignedIn
+    };
 }
 
 export default connect(
